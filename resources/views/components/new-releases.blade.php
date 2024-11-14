672eabf2221b5974f982bfa53b625a691df07e70
RizkyMaulanaS <rizkyzky345@gmail.com>
2024-11-14 07:22:50 +0700

    remove: line code ini blade

--- resources/views/components/new-releases.blade.php
+++ resources/views/components/new-releases.blade.php
@@ -1,14 +1,12 @@
-@if (isset($newReleases->albums->items))
+<div>
+    <p>Album Terbaru</p>
     <div>
-        <h1>Album Terbaru</h1>
-        <div>
-            @foreach($newReleases->albums->items as $album)
-                <div>
-                    <img src="{{ $album->images[0]->url }}" alt="{{ $album->name }}" width="100">
-                    <p>{{ $album->name }}</p>
-                    <p>Artis: {{ $album->artists[0]->name }}</p>
-                </div>
-            @endforeach
-        </div>
+        @foreach($newReleases->albums->items as $album)
+            <div>
+                <img src="{{ $album->images[0]->url }}" alt="{{ $album->name }}" width="100">
+                <p>{{ $album->name }}</p>
+                <p>Artis: {{ $album->artists[0]->name }}</p>
+            </div>
+        @endforeach
     </div>
-@endif
+</div>
