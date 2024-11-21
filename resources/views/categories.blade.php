f25bd2d256f621e00951ab3aefa0e1a921414361
RizkyMaulanaS <rizkyzky345@gmail.com>
2024-11-21 20:31:03 +0700

    conf:categories, playlist, header & sidebar

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
+    <title>Categories</title>
     @vite('resources/css/app.css')
 </head>
 <body class="bg-BGHome">
     <x-header/>
     <x-sidebar/>
-    <div class="container bg-hover rounded-xl mt-20 ml-60 p-3 w-96">
+    <div class="container bg-hover rounded-xl mt-20 ml-60 p-3 w-auto">
         <h2 class="text-2xl font-bold text-white mb-6">Categories</h2>
-        <div class="grid grid-cols-2 gap-6">
+        <div class="grid grid-cols-4 gap-6">
             <div class="bg-gray-800 p-4 rounded-xl shadow-lg hover:bg-gray-700 transform transition">
                 <p class="text-white text-lg font-semibold">Pop</p>
             </div>
--- resources/views/components/feat-playlist.blade.php
+++ resources/views/components/feat-playlist.blade.php
@@ -1,5 +1,5 @@
-<div class="container mx-auto bg-hover rounded-xl mt-20 ml-64">
-    <h2 class="px-2 text-2xl font-bold mt-5 text-white ">Playlist Unggulan</h2>
+<div class="container mx-auto bg-hover rounded-xl mt-20 ml-60 w-auto">
+    <h2 class="px-2 text-2xl font-bold text-white">Playlist Unggulan</h2>
     {{-- <div class="flex items-center mb-2 mt-2">
         <button id="scroll-left" class="mx-2 text-white bg-gray-600 p-3 rounded-md hover:bg-gray-500">
             &lt;
@@ -11,7 +11,7 @@
     
     <div id="playlist-container" class="flex overflow-x-scroll whitespace-nowrap space-x-5 py-4">
         @foreach($featPlaylist->playlists->items as $playlist)
-            <div class="w-36 rounded-xl p-4 shadow-lg hover:scale-105 transform transition">
+            <div class="w-40 rounded-xl p-3 shadow-lg hover:scale-105 transform transition">
                 <img class="rounded-lg mb-4" src="{{ $playlist->images[0]->url }}" alt="{{ $playlist->name }}" width="100">
                 <p class="text-white text-base font-semibold">{{ $playlist->name }}</p>
             </div>
